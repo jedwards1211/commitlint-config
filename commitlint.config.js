43124a4734eefe5a9ae86368f5f6eebcd3e678da
Andy Edwards <jedwards@fastmail.com>
2020-10-26 10:38:47 -0500

    fix: relax subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-case': [0, 'always', ['lowerCase']],
+    'subject-case': [2, 'never', ['upper-case', 'camel-case', 'pascal-case', 'snake-case']],
   },
 }
